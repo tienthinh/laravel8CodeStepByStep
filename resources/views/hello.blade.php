81faeec81c29caaa546fbe1d7453689b722a1837
tienthinh <p.tienthinh@gmail.com>
2023-10-03 09:14:44 +0700

    make the first file

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Basic Web Page</title>
+        <style>
+            body {
+                background-color: #2D2D2D;
+            }
+            
+            h1 {
+                color: #C26356;
+                font-size: 30px;
+                font-family: Menlo, Monaco, fixed-width;
+            }
+            
+            p {
+                color: white;
+                font-family: "Source Code Pro", Menlo, Monaco, fixed-width;
+            }
+        </style>
+    </head>
+    <body>
+        <p>Hello World!</p>
+    </body>
+</html>
